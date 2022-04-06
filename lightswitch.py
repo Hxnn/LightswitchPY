ee28a8ade82c0e3520b1fecae63d8494671865b3
Hinn <64025546+Hxnn@users.noreply.github.com>
2022-04-06 11:20:22 +0200

    Lightswitch part1

--- lightswitch.py
+++ lightswitch.py
@@ -1,13 +1,30 @@
 import tkinter as tk
+
 window = tk.Tk()
-
-button = tk.Button(text='...', bg="white", fg="black")
-button.pack(pady = 20, padx = 20)
+window.geometry('300x300')
 
 # schijf hier tussen je code
+x = True
 
 
+def Lightswitch():
+    global x
+    if x:
+        print('Light on')
+        x = False
+        button.configure(text="Switch light off =)")
+        window.configure(bg="yellow")
+    else:
+        print('Light off')
+        x = True
+        button.configure(text="Switch light on =)")
+        window.configure(bg="black")
 
+
+button = tk.Button(text='Switch light on =)', bg='white', fg='black', command=Lightswitch)
+
+button.pack()
 # schijf hier tussen je code
+
 
 window.mainloop()
